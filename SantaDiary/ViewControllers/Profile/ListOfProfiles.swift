890ee88f85ba4e1c703d4a9d78c140cc695cefcc
Brian Veitch <beveitch@gmail.com>
2022-01-24 09:11:55 -0600

    make it clear

--- SantaDiary/ViewControllers/Profile/ListOfProfiles.swift
+++ SantaDiary/ViewControllers/Profile/ListOfProfiles.swift
@@ -79,10 +79,9 @@ class ListOfProfiles: UIViewController {
     
     func setupTableView() {
         
-        // need this!
+        // make sure this is .clear!!
         tableView.backgroundColor = .clear
 
-        
         // connect to tableview cell
         let nib = UINib(nibName: ListOfProfilesCell.identifer, bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: ListOfProfilesCell.identifer)
